c442f2f2c0e3f1a0ba3692b2091c54ad3f4ba333
golucyfrid <golu@cyfrid.com>
2025-10-14 16:40:55 +0530

    Horoscope, gemstones-enquiries pages html create and design

--- src/pages/horoscope-details.tsx
+++ src/pages/horoscope-details.tsx
@@ -1,7 +1,7 @@
 import { genestroImg } from "@/assets/images"
 import Image from "next/image"
 
-const GemsonesEnquirires = () => {
+const HoroscopeDetails = () => {
 	// useEffect(() => {
 	// gemsService.getGemsInquiries().then((result: any) => {
 	// console.log('result', result);
@@ -22,19 +22,64 @@ const GemsonesEnquirires = () => {
 				<div className="container pt-4">
 					<div className="row">
 						<div className="col-12">
+							<div className="card mb-2">
+								<div className="mb-3">
+									<Image
+										src={genestroImg}
+										alt="Gemstons"
+										width={100}
+										height={100}
+										className="img-fluid rounded-2 shadow-lg bg-white mb-2 d-flex justify-content-center w-75 h-75 mx-auto p-4"
+									/>
+								</div>
 
-							<div className="card rounded-2 shadow-lg bg-white mb-2 position-relative">
-								<Image
-									src={genestroImg}
-									alt="Gemstons"
-									width={100}
-									height={100}
-									className="img-fluid w-100 rounded-2"
-								/>
-								<button type="button" className="bg-white border-0 rounded-2 p-2 mt-1 me-1 position-absolute top-0 end-0">
-									<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="20" height="20" x="0" y="0" viewBox="0 0 24 24"><g><path fill="#328fe0" d="m16.004 9.414-8.607 8.607-1.414-1.414L14.589 8H7.004V6h11v11h-2z" opacity="1" data-original="#000000"></path></g></svg>
-								</button>
-								<div className="card-body">
+								<div className="card-body rounded-2 shadow-lg bg-white mb-2">
+									<h4 className="mb-2">Enquirie Details</h4>
+									<div className="d-flex justify-content-between mb-2">
+										<div className="user-info">
+											<h6 className="mb-0">Inquiry
+											</h6>
+											<p className="mb-0"><small>05826</small></p>
+										</div>
+
+										<div className="user-info">
+											<h6 className="mb-0">Gemstones
+											</h6>
+											<p className="mb-0"><small>Natural Opal</small></p>
+										</div>
+
+									</div>
+
+									<div className="d-flex justify-content-between mb-2">
+										<div className="user-info">
+											<h6 className="mb-0">Create date
+											</h6>
+											<p className="mb-0"><small>13 oct 2025 14:23</small></p>
+										</div>
+
+										<div className="user-info">
+											<h6 className="mb-0">Status
+											</h6>
+											<p className="mb-0"><small>New</small></p>
+										</div>
+
+									</div>
+
+								</div>
+							</div>
+							<div className="card mb-2">
+								<div className="mb-3">
+									<Image
+										src={genestroImg}
+										alt="Gemstons"
+										width={100}
+										height={100}
+										className="img-fluid rounded-2 shadow-lg bg-white mb-2 d-flex justify-content-center w-75 h-75 mx-auto p-4"
+									/>
+								</div>
+
+								<div className="card-body rounded-2 shadow-lg bg-white mb-2">
+									<h4 className="mb-2">Enquirie Details</h4>
 									<div className="d-flex justify-content-between mb-2">
 										<div className="user-info">
 											<h6 className="mb-0">Inquiry
@@ -69,7 +114,7 @@ const GemsonesEnquirires = () => {
 							</div>
 
 
-							<div className="card rounded-2 shadow-lg bg-white mb-2 enquirires-placehoder-card">
+							{/* <div className="card rounded-2 shadow-lg bg-white mb-2 enquirires-placehoder-card">
 								<div className="placeholder-glow">
 									<span className="placeholder placehoder-img-enquires col-12 rounded-2"></span>
 								</div>
@@ -94,7 +139,7 @@ const GemsonesEnquirires = () => {
 										</div>
 									</div>
 
-								<div className="d-flex justify-content-between mb-2">
+									<div className="d-flex justify-content-between mb-2">
 										<div className="user-info user-info-placehoder">
 											<h6 className="mb-0 placeholder-glow">
 												<span className="placeholder col-12 inquiry-placehoder"></span>
@@ -115,9 +160,54 @@ const GemsonesEnquirires = () => {
 									</div>
 
 								</div>
+							</div> */}
+
+
+						</div>
+
+						<div className="col-12">
+							<div className="card px-2">
+								<div className="card-body pb-5">
+
+								<h5>Comments</h5>
+								<p className="text-center">No comments found</p>
+								</div>
+								<div className="card-footer bg-white border-0 px-0">
+									<div className="mb-3 form-group position-relative">
+										<input
+											type="text"
+											className="form-control"
+											id="formGroupExampleInput"
+											placeholder="Add your comments"
+										/>
+										<button
+											type="button"
+											className="border-0 position-absolute top-50 end-0 translate-middle-y me-2 p-0"
+										>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												version="1.1"
+												width="18"
+												height="18"
+												x="0"
+												y="0"
+												viewBox="0 0 24 25"
+											>
+												<g>
+													<path
+														fill="#328fe0"
+														fillRule="evenodd"
+														d="M12.293 4.295a1 1 0 0 1 1.414 0l7 7a1 1 0 0 1 0 1.414l-7 7a1 1 0 0 1-1.414-1.414l5.293-5.293H4a1 1 0 1 1 0-2h13.586L12.293 5.71a1 1 0 0 1 0-1.414z"
+														clipRule="evenodd"
+														opacity="1"
+													></path>
+												</g>
+											</svg>
+										</button>
+									</div>
+
+								</div>
 							</div>
-
-
 						</div>
 					</div>
 				</div>
@@ -126,4 +216,4 @@ const GemsonesEnquirires = () => {
 		</>
 	)
 }
-export default GemsonesEnquirires
+export default HoroscopeDetails
--- src/pages/horoscope/[id].tsx
+++ src/pages/horoscope/[id].tsx
@@ -1,10 +1,69 @@
+import { genestroImg } from "@/assets/images"
 import { SEOHead } from "@/components"
+import Image from "next/image"
 
-const HoroscopeDetails=()=>{
-  return(
+const HoroscopeDetails = () => {
+  return (
     <>
-    <SEOHead title={'Horoscope Details'}/>
-    thsi sis horooscop detaila page
+      <SEOHead title={'Horoscope Details'} />
+
+      <section className="horoscope-details">
+        <div className="card-header bg-white border-bottom p-3 sticky-sidebar">
+          <h5 className="card-title text-black d-flex align-items-center gap-2">
+            <a href="#">
+              <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="23" height="20" x="0" y="0" viewBox="0 0 24 24" >
+                <g><path d="M21 11H5.414l5.293-5.293a1 1 0 1 0-1.414-1.414l-7 7a1 1 0 0 0 0 1.414l7 7a1 1 0 0 0 1.414-1.414L5.414 13H21a1 1 0 0 0 0-2z" fill="#000000" opacity="1" data-original="#000000"></path></g></svg></a>
+            Horoscope Details</h5>
+        </div>
+        <div className="container">
+          <div className="row">
+
+
+            <div className="col-12">
+
+              <div className="details-card mt-3">
+                <h6 className="mb-2">Raj Darwar</h6>
+                <p className="mb-0"><strong>Gender:</strong> Male</p>
+                <p className="mb-0"><strong>DOB:</strong> 2011-10-14</p>
+                <p className="mb-0"><strong>TOB:</strong> 13:20:00</p>
+                <p className="mb-0"><strong>Place:</strong> Jaipur, Rajasthan, India</p>
+
+                <div className="horoscope-box my-2">
+                  <Image
+                    src={genestroImg}
+                    alt="Horoscope Chart"
+                    width={100}
+                    height={100}
+                    className="img-fluid w-100 rounded-2 shadow-lg bg-white mb-2 d-flex justify-content-center  p-2"
+                  />
+                </div>
+
+                <div className="mahadasa-section mt-4">
+                  <h5>Mahadasha</h5>
+
+                  <div className="mahadasa-card">
+                    <h6 className="text-center">Mahadasha 1</h6>
+                    <p className="d-flex justify-content-between mb-1"><strong>Planet:</strong> सूर्य</p>
+                    <p className="d-flex justify-content-between mb-1"><strong>Start:</strong> 6-6-2020 21:23</p>
+                    <p className="d-flex justify-content-between mb-1"><strong>End:</strong> 7-6-2026 9:23</p>
+                  </div>
+
+                  <div className="mahadasa-card">
+                    <h6 className="text-center">Mahadasha 2</h6>
+                    <p className="d-flex justify-content-between mb-1"><strong>Planet:</strong>
+                      <span>शुक्र</span>
+                    </p>
+                    <p className="d-flex justify-content-between mb-1"><strong>Start:</strong> 7-6-2025 3:23</p>
+                    <p className="d-flex justify-content-between mb-1"><strong>End:</strong> 7-6-2038 9:23</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
     </>
   )
 }
